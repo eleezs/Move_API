d021c1e7337b6bc679e31eec2cbd984f4d500bc5
eleezs <wilduche@gmail.com>
2023-12-26 11:23:54 +0100

    setup, create and view

---
+++ movies/src/main/java/dev/elizs/movies/MovieService.java
@@ -0,0 +1,29 @@
+package dev.elizs.movies;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+
+  @Autowired //This instantiates the class MovieRepository
+  private MovieRepository movieRepository;
+
+  public List<Movie> allMovies() {
+    return movieRepository.findAll();
+  }
+
+  // get by id
+  public Optional<Movie> singleMovie(ObjectId id) {
+    return movieRepository.findById(id);
+  }
+
+  public Optional<Movie> singleMovieByImdbId(String imdbId) {
+    return movieRepository.findMovieByImdbId(imdbId);
+  }
+
+}
