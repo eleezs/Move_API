9e7473ff1601661add4d7baf84fd19530c15f2cd
eleezs <wilduche@gmail.com>
2024-06-24 11:31:08 +0100

    deploy

--- movies/src/main/java/dev/elizs/movies/MovieService.java
+++ movies/src/main/java/dev/elizs/movies/MovieService.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+  
 @Service
 public class MovieService {
 
